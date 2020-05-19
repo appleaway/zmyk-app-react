f0261c304ca7ba0a9b356c520c0bbc17c578922a
Away <785606282@qq.com>
2020-05-19 16:09:32 +0800

    前端接口请求文件

---
+++ src/request/api.ts
@@ -0,0 +1,97 @@
+import instance from "./index";
+
+/**
+ * 首页轮播图数据
+ */
+export function getSwiper() {
+  return instance({
+    url: "/swiper"
+  });
+}
+
+/**
+ * 首页楼层数据
+ */
+export function getComicFloor() {
+  return instance({
+    url: "/floor"
+  });
+}
+
+/**
+ * 排行榜数据（人气，打赏，月票）
+ */
+export function getRanklist() {
+  return instance({
+    url: "/ranklist"
+  });
+}
+
+/**
+ * 排行榜数据（人气，打赏，月票）
+ * @param {string} id 漫画id
+ */
+export function getBookDetail(id: string) {
+  return instance({
+    url: "/bookdetail",
+    params: {
+      id
+    }
+  });
+}
+
+/**
+ * 漫画的章节数据
+ * @param {string} id 漫画id
+ */
+export function getBookchapters(id: string) {
+  return instance({
+    url: "/bookchapters",
+    params: {
+      id
+    }
+  });
+}
+
+/**
+ * 当前漫画的相关推荐
+ * @param {string} bookId 漫画id
+ */
+export function getBookCorrelation(bookId: string) {
+  return instance({
+    url: "/correlation",
+    params: {
+      bookId
+    }
+  });
+}
+
+/**
+ * 当前漫画章节的图片
+ * @param {string} bookId 漫画id
+ * @param {string} chapterId 章节id
+ */
+export function getChapterPic({
+  bookId,
+  chapterId
+}: {
+  bookId: string;
+  chapterId: string;
+}) {
+  return instance({
+    url: "/chapterPic",
+    params: {
+      bookId,
+      chapterId
+    }
+  });
+}
+
+/**
+ * 获取类别
+ */
+export function getCategory() {
+  return instance({
+    url: "/category"
+  });
+}
---
+++ src/request/index.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+// axios.defaults.withCredentials = true;
+
+const instance = axios.create();
+
+instance.interceptors.request.use(
+  function onFulfilled(config) {
+    if (process.env.NODE_ENV === "development") {
+      console.groupCollapsed(`请求: ${config.url}`);
+      console.log(config);
+      console.groupEnd();
+    }
+    return config;
+  },
+  function onRejected(error) {
+    //
+  }
+);
+
+instance.interceptors.response.use(
+  function onFulfilled(response) {
+    if (process.env.NODE_ENV === "development") {
+      console.groupCollapsed(`响应: ${response.config.url}`);
+      console.log(response);
+      console.groupEnd();
+    }
+    return response;
+  },
+  function onRejected(error) {
+    //
+  }
+);
+
+export default instance;

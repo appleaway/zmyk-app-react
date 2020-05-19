fecc7dd584a4703523eb622c4abe501044ff1c40
Away <785606282@qq.com>
2020-05-19 16:29:28 +0800

    FooterNav组件修改NavLink为push方式

--- src/components/FooterNav/FooterNav.tsx
+++ src/components/FooterNav/FooterNav.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import "./FooterNav.css"
-import { NavLink, useRouteMatch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function FooterNav() {
-
-  const match = useRouteMatch();
-
-  console.log("match", match)
 
   return (
     <>
       <div className="blank"></div>
       <ul className="mk-footernav">
         <li className="item">
-          <NavLink to="/home" replace >
+          <NavLink to="/home"  >
             <div className="icon">
               <i className="ift-home"></i>
             </div>
             <span className="text">首页</span>
           </NavLink >
         </li>
-        <li className="item stop">
-          <NavLink to="/sort" replace title="分类">
+        <li className="item">
+          <NavLink to="/sort" title="分类">
             <div className="icon">
               <i className="ift-class-outline"></i>
             </div>
@@ -29,15 +25,15 @@ export default function FooterNav() {
           </NavLink>
         </li>
         <li className="item">
-          <NavLink to="/update" replace title="更新">
+          <NavLink to="/update" title="更新">
             <div className="icon">
               <i className="ift-find-outline"></i>
             </div>
             <span className="text">更新</span>
           </NavLink>
         </li>
-        <li className="item">
-          <NavLink to="/uc" replace title="我的">
+        <li className="item stop">
+          <NavLink to="/uc" title="我的">
             <div className="icon">
               <i className="ift-mine-outline"></i>
             </div>

197950d382fa29d24f76e34362074e5e311e0af1
Away <785606282@qq.com>
2020-05-19 16:50:46 +0800

    FooterNav默认匹配path与routes中的path对应选中

--- src/components/FooterNav/FooterNav.tsx
+++ src/components/FooterNav/FooterNav.tsx
@@ -1,45 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./FooterNav.css"
-import { NavLink } from 'react-router-dom';
+import { NavLink, useRouteMatch } from 'react-router-dom';
+
+const defaultFooterNav = [{
+  path: "/home",
+  name: "首页",
+  activeIconClass: "ift-home",
+  inActiveIconClass: "ift-home-outline",
+}, {
+  path: "/category",
+  name: "分类",
+  activeIconClass: "ift-class",
+  inActiveIconClass: "ift-class-outline",
+}, {
+  path: "/update",
+  name: "更新",
+  activeIconClass: "ift-find",
+  inActiveIconClass: "ift-find-outline",
+}, {
+  path: "/mine",
+  name: "我的",
+  activeIconClass: "ift-mine",
+  inActiveIconClass: "ift-mine-outline",
+}]
 
 export default function FooterNav() {
+
+  const { path } = useRouteMatch();
+  const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    setCurrentPath(path)
+  }, [path])
 
   return (
     <>
       <div className="blank"></div>
       <ul className="mk-footernav">
-        <li className="item">
-          <NavLink to="/home"  >
-            <div className="icon">
-              <i className="ift-home"></i>
-            </div>
-            <span className="text">首页</span>
-          </NavLink >
-        </li>
-        <li className="item">
-          <NavLink to="/sort" title="分类">
-            <div className="icon">
-              <i className="ift-class-outline"></i>
-            </div>
-            <span className="text">分类</span>
-          </NavLink>
-        </li>
-        <li className="item">
-          <NavLink to="/update" title="更新">
-            <div className="icon">
-              <i className="ift-find-outline"></i>
-            </div>
-            <span className="text">更新</span>
-          </NavLink>
-        </li>
-        <li className="item stop">
-          <NavLink to="/uc" title="我的">
-            <div className="icon">
-              <i className="ift-mine-outline"></i>
-            </div>
-            <span className="text">我的</span>
-          </NavLink>
-        </li>
+        {
+          defaultFooterNav.map((item, index) => {
+            return (
+              <li className="item" key={index}>
+                <NavLink to={item.path}  >
+                  <div className="icon">
+                    <i className={currentPath === item.path ? item.activeIconClass : item.inActiveIconClass}></i>
+                  </div>
+                  <span className="text">{item.name}</span>
+                </NavLink >
+              </li>
+            )
+          })
+        }
       </ul>
     </>
   )

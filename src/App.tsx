2d5427352bb427c1e0afe96c00c7a08e6c96d74f
Away <785606282@qq.com>
2020-05-19 16:03:01 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+// import routes from "./router/index"
+// import RouterView from './components/RouterView/RouterView';
+
+// function App() {
+//   return (
+//     <RouterView routes={routes} from="/" to="/home" />
+//   );
+// }
+
+function App() {
+  return <h1>App </h1>
+}
+
+export default App;
